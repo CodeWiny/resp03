aff4c9922b1ba94fd775279c16d5272ff391fc0b
Admin <1473810815@qq.com>
2020-04-10 12:42:04 +0800

    添加方法

--- myfirstgit/src/com/firstgit/FirstGit.java
+++ myfirstgit/src/com/firstgit/FirstGit.java
@@ -9,4 +9,9 @@ public class FirstGit {
         System.out.println("和咯哦 gti git");
         System.out.println("和咯哦 gti git");
     }
+    public void  Test(){
+        int i=0;
+        i+=10;
+    }
+
 }

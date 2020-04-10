0e39da6341ee3d607330e817a93a28ed512ac4d9
Admin <1473810815@qq.com>
2020-04-10 12:40:16 +0800

    提交项目到git

---
+++ myfirstgit/src/com/firstgit/FirstGit.java
@@ -0,0 +1,12 @@
+package com.firstgit;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("和咯哦 gti git");
+        System.out.println("和咯哦 gti git");
+        System.out.println("和咯哦 gti git");
+        System.out.println("和咯哦 gti git");
+        System.out.println("和咯哦 gti git");
+        System.out.println("和咯哦 gti git");
+    }
+}
